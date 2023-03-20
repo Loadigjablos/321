86cd99210926e9df5ee66abe282ccdbdaa4e408b
Loadigjablos <loadingjablowme@gmail.com>
2023-03-20 15:57:17 +0100

    mongodb connected (should work)

--- server/database/main.js
+++ server/database/main.js
@@ -1,74 +1,49 @@
-let pool = null;
 
-const initializeMariaDB = async () => {
-  const mariadb = require("mariadb");
-  pool = mariadb.createPool({
-    database: process.env.DB_NAME || "mychat",
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "supersecret123",
-    port: 3306,
-    //connectionLimit: 5,
-  });
-};
-
-const executeSQL = async (query, variables = null) => {
-  let conn;
+/**
+ * connects to mongodb
+ * @param mogofunc this is a function that will get executed using the mongo client as a parameter
+ * source: https://www.mongodb.com/developer/languages/javascript/node-connect-mongodb/
+ */
+const executemongoDBFunction = async (mogoFunc) => {
+  let result;
+  const { MongoClient } = require("mongodb");
+  const uri = "mongodb://localhost:27017";
+  const client = new MongoClient(uri);
   try {
-    conn = await pool.getConnection();
-    const res = await conn.query(query, variables);
-    conn.end();
-    return res;
+    await client.connect();
+
+    // The provided function gets called
+    result = await mogoFunc(client);
   } catch (err) {
-    try {
-      conn.end();
-    } catch (e) {}
-    console.log(err);
+    console.error(err);
+  } finally {
+    await client.close();
+    return result;
   }
-  return false;
 };
 
 const initializeDBSchema = async () => {
-    const userTableQuery = `CREATE TABLE IF NOT EXISTS users (
-      id INT NOT NULL AUTO_INCREMENT,
-      name VARCHAR(255) NOT NULL,
-      password VARCHAR(255) NOT NULL,
-      PRIMARY KEY (id),
-      UNIQUE KEY (name)
-    );`;
-    await executeSQL(userTableQuery);
-
-    const messageTableQuery = `CREATE TABLE IF NOT EXISTS publicmessages (
-      id INT NOT NULL AUTO_INCREMENT,
-      user_name VARCHAR(255) NOT NULL,
-      message VARCHAR(255) NOT NULL,
-      PRIMARY KEY (id),
-      FOREIGN KEY (user_name) REFERENCES users(name)
-    );`;
-    await executeSQL(messageTableQuery);
-
-    const groupChatsQuery = `CREATE TABLE IF NOT EXISTS groupchats (
-      id INT NOT NULL AUTO_INCREMENT,
-      name VARCHAR(255) NOT NULL,
-      admin VARCHAR(255) NOT NULL,
-      users VARCHAR(65536) NOT NULL,
-      PRIMARY KEY (id),
-      FOREIGN KEY (admin) REFERENCES users(name)
-    );`;
-    await executeSQL(groupChatsQuery);
-    
+  const users = (client) => {
+    return listDatabases(client);
   };
+  await executemongoDBFunction(users);
 
-  const {
-    registerNewUser, deleteUserbyName, getOneUserById,
-    getOneUserByName, getAllUsers 
-  } = require("./user");
+  const publicmessages = (client) => {
+    return listDatabases(client);
+  };
+  await executemongoDBFunction(publicmessages);
 
-  const {} = require("./groupchat");
+  const groupchats = (client) => {
+    return listDatabases(client);
+  };
+  await executemongoDBFunction(groupchats);
+};
+
+const {} = require("./user");
+
+const {} = require("./groupchat");
 
 module.exports = {
-    pool,
-    executeSQL, initializeMariaDB, initializeDBSchema,
-    registerNewUser, deleteUserbyName, getOneUserById,
-    getOneUserByName, getAllUsers 
+  executemongoDBFunction,
+  initializeDBSchema,
 };
