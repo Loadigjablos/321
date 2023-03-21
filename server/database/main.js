802d88d65ce0ab9d10598cd186dbe15a88971281
Loadigjablos <loadingjablowme@gmail.com>
2023-03-21 09:43:22 +0100

    trying to connnect to mongodb

--- server/database/main.js
+++ server/database/main.js
@@ -1,30 +1,65 @@
-
 /**
  * connects to mongodb
  * @param mogofunc this is a function that will get executed using the mongo client as a parameter
  * source: https://www.mongodb.com/developer/languages/javascript/node-connect-mongodb/
  */
+
 const executemongoDBFunction = async (mogoFunc) => {
   let result;
   const { MongoClient } = require("mongodb");
-  const uri = "mongodb://localhost:27017";
-  const client = new MongoClient(uri);
+  const URI = "mongodb://127.0.0.1:27017/mychat";
+  const CLIENT = new MongoClient(URI);
   try {
-    await client.connect();
+    await CLIENT.connect();
 
     // The provided function gets called
-    result = await mogoFunc(client);
+    result = await mogoFunc(CLIENT);
   } catch (err) {
     console.error(err);
   } finally {
-    await client.close();
+    await CLIENT.close();
     return result;
   }
 };
 
+/**
+ * source: https://adrianmejia.com/creating-a-restful-api-tutorial-with-nodejs-and-mongodb/
+ */
+/*
+const executemongoDBFunction = async (mogoFunc) => {
+  var mongoose = require('mongoose');
+
+  const URI = "mongodb://localhost:27017/mychat";
+  let MongoDB = await mongoose.connect(URI).connection;
+
+  result = await mogoFunc(MongoDB);
+}
+
+/**
+ * 
+ */
 const initializeDBSchema = async () => {
   const users = (client) => {
-    return listDatabases(client);
+    // schema for One User
+    var user = new client.Schema({
+      name: String,
+      password: String,
+      created: { type: Date, default: Date.now },
+    });
+
+    // Create a model based on the schema
+    const USERS_MODEL = client.model("users", user);
+
+    const newuser = new user({ name: "James", password: "pp" });
+
+    // Save it to database
+    newuser.save(function (err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(newuser);
+      }
+    });
   };
   await executemongoDBFunction(users);
 
@@ -47,5 +82,3 @@ module.exports = {
   executemongoDBFunction,
   initializeDBSchema,
 };
-
-
