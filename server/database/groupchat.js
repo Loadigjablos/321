417b314a0dd5f249e9b6e34c289a3bc7280b77ef
Loadigjablos <loadingjablowme@gmail.com>
2023-03-24 00:35:40 +0100

    added group chat functions

--- server/database/groupchat.js
+++ server/database/groupchat.js
@@ -1,41 +1,56 @@
 const { executeSQL } = require("./main");
 
 const createNewGroup = async (name, users) => {
-    const groupChatName = "groupchat_" + name;
+  const groupChatName = "groupchat_" + name;
 
-    const usersString = JSON.stringify(users);
+  const usersString = JSON.stringify(users);
 
-    console.log(groupChatName, usersString);
+  const newGroupChatQueryInsert = `INSERT INTO groupchats (id, users, name) VALUES (NULL, '${name}', '${usersString}');`;
 
-    const newGroupChatQueryInsert = `INSERT INTO groupchats (id, users, name) VALUES (NULL, '${name}', '${usersString}');`;
-  
-    const newGroupChatQueryDB = `CREATE TABLE '${groupChatName}' (id INT NOT NULL AUTO_INCREMENT , user_name VARCHAR(255) NOT NULL , message TEXT NOT NULL , PRIMARY KEY (id))`;
+  const newGroupChatQueryDB = `CREATE TABLE '${groupChatName}' (id INT NOT NULL AUTO_INCREMENT , username VARCHAR(255) NOT NULL , message TEXT NOT NULL , PRIMARY KEY (id))`;
 
-    await executeSQL(newGroupChatQueryInsert);
-    await executeSQL(newGroupChatQueryDB);
-}
+  await executeSQL(newGroupChatQueryInsert);
+  await executeSQL(newGroupChatQueryDB);
+};
 
 //createNewGroup("jeffry", ["hi", "james"]);
 
 const deleteGroup = async (name) => {
-    const groupChatName = "groupchat_" + name;
+  const groupChatName = "groupchat_" + name;
+};
 
+const joinGroupp = async (user, groupName) => {
+  // alter groupchats table with new user
 }
 
 const getGroupMessages = async (user) => {
   const groups = `SELECT * FROM groups`;
   let allMessages = [];
   for (group in groups) {
+    for (chatMember in JSON.parse(group.users)) {
+      if ((user = chatMember)) {
+        const databaseName = "groupchat_" + group.name;
 
+        const query = `SELECT * FROM ${databaseName}`;
+        const allMessagesFromThisGroup = await executeSQL(query);
 
-    allMessages.push();
+        const groupChatJSON = {
+          name: group.name,
+          members: group.users,
+          messages: allMessagesFromThisGroup,
+        };
+
+        allMessages.push(groupChatJSON);
+      }
+    }
   }
   return allMessages;
   //const allMessages = await executeSQL(users);
-}
+};
 
-const listAllGroups = async () => {
-
-}
-  
-module.exports = { createNewGroup, getGroupMessages, deleteGroup, listAllGroups };
+module.exports = {
+  createNewGroup,
+  getGroupMessages,
+  deleteGroup,
+  joinGroupp,
+};
